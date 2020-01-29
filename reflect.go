b4db7a498bc564d87a93d10718852caf33ea511e
Saeed <saeid.kamyabi71@gmail.com>
2020-01-29 18:22:31 +0330

    add reflect.Clone function

--- reflect.go
+++ reflect.go
@@ -95,3 +95,14 @@ func IsEmptyValue(v reflect.Value) bool {
 	}
 	return false
 }
+
+func Clone(inter interface{}) interface{} {
+	nInter := reflect.New(reflect.TypeOf(inter).Elem())
+	val := reflect.ValueOf(inter).Elem()
+	nVal := nInter.Elem()
+	for i := 0; i < val.NumField(); i++ {
+		nvField := nVal.Field(i)
+		nvField.Set(val.Field(i))
+	}
+	return nInter.Interface()
+}
